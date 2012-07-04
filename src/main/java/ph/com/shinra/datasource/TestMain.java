71549ee06152e4f0eb148809c7a7785e326dd5ac
Edwin Bautista <edwin.bautista@cubepoint.be>
2012-07-04 11:55:02 +0800

    Temporary solution on getting a singleton application context while the client side framework has not yet been decided.

--- src/main/java/ph/com/shinra/datasource/TestMain.java
+++ src/main/java/ph/com/shinra/datasource/TestMain.java
@@ -2,24 +2,19 @@ package ph.com.shinra.datasource;
 
 import java.util.List;
 
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
-
 import ph.com.shinra.datasource.admin.model.Department;
 import ph.com.shinra.datasource.admin.service.DepartmentService;
 import ph.com.shinra.datasource.common.service.GenericService;
+import ph.com.shinra.datasource.utils.ApplicationContextUtils;
 
 public class TestMain {
 	
 	@SuppressWarnings("unchecked")
 	public static void main(String[] args) {
-		String[] contextXml = new String[]{ "hibernate-applicationContext.xml", "datasource-applicationContext.xml" };
-		ApplicationContext ctx = new ClassPathXmlApplicationContext(contextXml);
-		
-		GenericService<Department, Long> deptService = (GenericService<Department, Long>) ctx.getBean("departmentService");
+		GenericService<Department, Long> deptService = (GenericService<Department, Long>) ApplicationContextUtils.getBean("departmentService");
 		deptService.create(new Department("Sub 3", "lkdhsdfbdfkdjgbdfgbjfbgkjbg", null));
 		
-		DepartmentService deptSearchService = (DepartmentService) ctx.getBean("departmentService");
+		DepartmentService deptSearchService = (DepartmentService) ApplicationContextUtils.getBean("departmentService");
 		List<Department> list = deptSearchService.search();
 		
 		for (Department department : list) {
---
+++ src/main/java/ph/com/shinra/datasource/utils/ApplicationContextUtils.java
@@ -0,0 +1,19 @@
+package ph.com.shinra.datasource.utils;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class ApplicationContextUtils {
+	
+	private static ApplicationContext context;
+	private static final String[] contextXml = new String[]{ "hibernate-applicationContext.xml", "datasource-applicationContext.xml" };
+	
+	public static ApplicationContext getContext() {
+		if (context == null) context = new ClassPathXmlApplicationContext(contextXml);
+		return context;
+	}
+
+	public static Object getBean(String beanName) {
+		return getContext().getBean(beanName);
+	}
+}
